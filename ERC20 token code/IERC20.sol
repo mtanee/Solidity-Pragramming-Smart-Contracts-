b4cd30498f157896e5612c47a1e5aeae450a6015
Taneer <114029687+mtanee@users.noreply.github.com>
2023-09-12 20:47:26 +0500

    Update IERC20.sol

--- ERC20 token code/IERC20.sol
+++ ERC20 token code/IERC20.sol
@@ -1,16 +1,16 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
 
-
-//Owner => Who decide total number of Token
-//Sender => who have token to spend
-//reciepient => who recieve Token
+pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20 {
-    function BalanceOf(address _spender) external view returns(uint);
-    function TotalSupply() external view returns(uint);
-    function TransferTo(address _receipt, uint _Amount) external returns(bool);
-    function TransferFrom(address _spender, address receipt, uint _amount) external;
-    function Allowance(address _owner,address _spender) external returns(uint);
-    function Approve(address _spender,uint _amount) external;  
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
+
